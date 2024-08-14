22092efced41a9df57d0b1fe99905f65f5d0efd8
mvrcoag <dev.nnqp2@slmails.com>
2024-08-14 12:42:24 -0600

    :feat :sparkles: file search improved

--- lua/mvrcoag/core/keymaps.lua
+++ lua/mvrcoag/core/keymaps.lua
@@ -26,7 +26,7 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") -- go prev tab
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle nvim tree
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>")
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files,-u<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
