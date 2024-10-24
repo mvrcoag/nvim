4c4fe3dc10f565f41bd51e03cccdbdd9d90672ba
mvrcoag <dev.nnqp2@slmails.com>
2024-10-24 16:36:24 -0600

    :feat alpha updated

--- lua/mvrcoag/plugins/alpha.lua
+++ lua/mvrcoag/plugins/alpha.lua
@@ -1,8 +1,27 @@
-return
-  {
-    config = function ()
-      local alpha = require("alpha")
-      local dashboard = require("alpha.themes.dashboard")
-      alpha.setup(dashboard.config)
-    end,
-  }
+return {
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			"mvrcoag",
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "Find file", ":Telescope find_files<CR>"),
+			dashboard.button("e", "New file", ":ene <BAR> startinsert<CR>"),
+			dashboard.button("p", "Find word", ":Telescope live_grep<CR>"),
+			dashboard.button("r", "Recently opened files", ":Telescope oldfiles<CR>"),
+			dashboard.button("c", "Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+			dashboard.button("q", "Quit", ":qa<CR>"),
+		}
+
+		dashboard.section.footer.opts.hl = "Type"
+		dashboard.section.header.opts.hl = "Include"
+		dashboard.section.buttons.opts.hl = "Keyword"
+
+		dashboard.opts.opts.noautocmd = true
+
+		alpha.setup(dashboard.config)
+	end,
+}
